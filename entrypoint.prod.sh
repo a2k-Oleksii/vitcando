6f2d51fac8c173c148e77695d4f7fe8f84d65daf
a2k-Oleksii <a2k-oleksii@ukr.net>
2023-01-24 21:53:20 +0200

    Create project with docker files and use nginx, gunicorn and postgresql. Version development and production

---
+++ entrypoint.prod.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres ..."
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    sleep 0.1
+  done
+  echo "Postgres started"
+fi
+
+exec "$@"
